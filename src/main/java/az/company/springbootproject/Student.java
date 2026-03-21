f11659c6bd6e6d5b90862b796c01461f9e77abe1
Ilkin <eyyuboffilkin@gmail.com>
2026-03-21 20:37:29 +0400

    Remove unused GetMapping import
    Removed unused import for GetMapping in Student.java

--- src/main/java/az/company/springbootproject/Student.java
+++ src/main/java/az/company/springbootproject/Student.java
@@ -5,13 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-=======
 
-import org.springframework.web.bind.annotation.GetMapping;
-
->>>>>>> 647a94d1d229f089bb73f4fa51b6a6199a368756
 @Data
 @Entity
 public class Student {
